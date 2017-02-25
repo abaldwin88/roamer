039e6b872fedd82eaa09c27c8f3aa95f3e42afd4
Alex Baldwin <alex@ecstaticlabs.com>
2017-02-24 20:29:04 -0500

    Move main into module

---
+++ roamer/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+argh
+"""
+
+import os
+from roamer.python_edit import file_editor
+from roamer.directory import Directory
+from roamer.edit_directory import EditDirectory
+from roamer.engine import Engine
+
+
+def main():
+    """
+    argh
+    """
+    cwd = os.getcwd()
+    raw_entries = os.listdir(cwd)
+    directory = Directory(cwd, raw_entries)
+    output = file_editor(directory.text())
+    edit_directory = EditDirectory(cwd, output)
+    diff_engine = Engine(directory, edit_directory)
+    print diff_engine.commands
+
+if __name__ == "__main__":
+    main()
