cdcd458f02ef916fdab4c1c3e5df345821bb977f
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-08 11:17:54 -0400

    Create test folder with fake entries

--- roamer/constant.py
+++ roamer/constant.py
@@ -6,9 +6,7 @@ import os
 ENTRIES_JSON_PATH = os.path.expanduser('~/.roamer-data/entries.json')
 TRASH_JSON_PATH = os.path.expanduser('~/.roamer-data/trash.json')
 TRASH_DIR = os.path.expanduser('~/.roamer-data/trash/')
-TEST_DIR = os.path.expanduser('~/lasdkfj/')
+TEST_DIR = os.path.expanduser(os.path.dirname(os.path.realpath(__file__)) + '/../tmp/test_dir/')
 
 if not os.path.exists(TRASH_DIR):
     os.makedirs(TRASH_DIR)
-if not os.path.exists(TEST_DIR):
-    os.makedirs(TEST_DIR)
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,12 +1,35 @@
-import roamer.main
+"""
+askldjf
+"""
+import os
+import shutil
+import unittest
+from roamer.main import Main
+from roamer.constant import TEST_DIR
 
 class TestStringMethods(unittest.TestCase):
-
     def setUp(self):
+        if os.path.exists(TEST_DIR):
+            shutil.rmtree(TEST_DIR)
+            os.makedirs(TEST_DIR)
+        else:
+            os.makedirs(TEST_DIR)
 
+        os.makedirs(os.path.join(TEST_DIR, 'hello/'))
+        with open(os.path.join(TEST_DIR, 'spam.txt'), "w") as text_file:
+            text_file.write('spam file content')
+        with open(os.path.join(TEST_DIR, 'eggs.txt'), "w") as text_file:
+            text_file.write('eggs file content')
+        with open(os.path.join(TEST_DIR, 'argh.txt'), "w") as text_file:
+            text_file.write('argh file content')
+        self.main = Main(TEST_DIR)
+        self.text = self.main.directory.text()
+
+    def process(self):
+        self.main.process(self.text)
 
     def tearDown(self):
-
+        shutil.rmtree(TEST_DIR)
 
     def test_upper(self):
         self.assertEqual('foo'.upper(), 'FOO')
