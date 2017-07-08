fb497ef97bd65930c1bdd2ade6074d2eb148beb0
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-08 09:57:51 -0400

    Add basic testing setup

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Plain Text File Explorer
 
 A broken work in process
+
+
+# Tests
+python -m unittest discover tests
---
+++ tests/test_integration.py
@@ -0,0 +1,26 @@
+import roamer.main
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+
+
+    def tearDown(self):
+
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
