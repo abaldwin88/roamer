61093bf8f80f384b160909931388f40f08f8c929
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-08 10:13:00 -0400

    Break apart main into separate class functions for easier testing

--- roamer/main.py
+++ roamer/main.py
@@ -13,24 +13,27 @@ from roamer.record import Record
 from roamer.constant import TRASH_DIR
 
 
-def main():
-    """
-    argh
-    """
-    if not os.path.exists(TRASH_DIR):
-        os.makedirs(TRASH_DIR)
+class Main(object):
+    def __init__(self, cwd=None):
+        if cwd is None:
+            self.cwd = os.getcwd()
+        if not os.path.exists(TRASH_DIR):
+            os.makedirs(TRASH_DIR)
+        raw_entries = os.listdir(self.cwd)
+        self.directory = Directory(self.cwd, raw_entries)
+        self.edit_directory = None
+        Record().add_dir(self.directory)
 
-    cwd = os.getcwd()
-    raw_entries = os.listdir(cwd)
+    def run(self):
+        output = file_editor(self.directory.text())
+        self.process(output)
 
-    directory = Directory(cwd, raw_entries)
-    Record().add_dir(directory)
-    output = file_editor(directory.text())
-    edit_directory = EditDirectory(cwd, output)
-    engine = Engine(directory, edit_directory)
-    print engine.print_commands()
-    engine.run_commands()
-    Record().add_dir(Directory(cwd, os.listdir(cwd)))
+    def process(self, output):
+        self.edit_directory = EditDirectory(self.cwd, output)
+        engine = Engine(self.directory, self.edit_directory)
+        print engine.print_commands()
+        engine.run_commands()
+        Record().add_dir(Directory(self.cwd, os.listdir(self.cwd)))
 
 if __name__ == "__main__":
-    main()
+    Main().run()
