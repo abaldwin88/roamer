0c8ce12e2a6513d245388c39abdd82ff0789c53b
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-16 13:15:40 -0400

    Fix empty string catch

--- roamer/edit_directory.py
+++ roamer/edit_directory.py
@@ -5,9 +5,6 @@ from collections import Counter
 from roamer.entry import Entry
 
 class EditDirectory(object):
-    """
-    argh
-    """
     def __init__(self, path, content):
         self.path = path
         self.entries = {}
@@ -41,10 +38,10 @@ class EditDirectory(object):
 def process_line(line):
     columns = line.split('|')
     name = columns[0]
-    if name[-1] == ' ':
-        name = name[:-1]
     if name.isspace() or name == '':
         name = None
+    elif name[-1] == ' ':
+        name = name[:-1]
 
     if len(columns) == 1:
         digest = None
