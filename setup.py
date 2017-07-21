cc89e5ceb260f3ad18e7d5754ce78396eb0f807d
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-21 08:22:19 -0400

    Add pypi setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'roamer',
+  packages = ['roamer'],
+  version = '0.0.2',
+  description = 'Plain Text File Explorer',
+  author = 'Alex Baldwin',
+  author_email = 'abaldwintech+hub@gmail.com',
+  url = 'https://github.com/abaldwin88/roamer',
+  download_url = 'https://github.com/abaldwin88/roamer/archive/v0.0.2.zip',
+  keywords = ['explorer', 'plain text', 'directory'], # arbitrary keywords
+  classifiers = [],
+)
