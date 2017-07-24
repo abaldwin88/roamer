e30cbeba09fb825ece1f2e24700f5c7fbf97aef3
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-23 20:11:15 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'roamer',
   packages = ['roamer'],
-  version = '0.0.2',
+  version = '0.0.3',
   description = 'Plain Text File Explorer',
   author = 'Alex Baldwin',
   author_email = 'abaldwintech+hub@gmail.com',
