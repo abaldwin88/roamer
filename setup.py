d99f59924871b480961b81852aa61c1f2eecd0c7
Alex Baldwin <alex@ecstaticlabs.com>
2017-07-23 20:14:17 -0400

    Version bump

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+version = '0.0.4'
 setup(
   name = 'roamer',
   packages = ['roamer'],
-  version = '0.0.3',
+  version = version,
   description = 'Plain Text File Explorer',
   author = 'Alex Baldwin',
   author_email = 'abaldwintech+hub@gmail.com',
   url = 'https://github.com/abaldwin88/roamer',
-  download_url = 'https://github.com/abaldwin88/roamer/archive/v0.0.2.zip',
+  download_url = 'https://github.com/abaldwin88/roamer/archive/v%s.zip' % version,
   keywords = ['explorer', 'plain text', 'directory'], # arbitrary keywords
   classifiers = [],
 )
